e237265811acb1ef98ac9dd4e30d95ed3451410c
BHouse3 <benjaminhouse@twc.com>
2025-12-29 17:00:57 -0500

    Timing optimizations. Antenna violation reductions.

--- openlane/config.tcl
+++ openlane/config.tcl
@@ -2,11 +2,13 @@ set ::env(DESIGN_NAME) top_hardened
 set ::env(VERILOG_FILES) [glob $::env(DESIGN_DIR)/src/*.v]
 set ::env(CLOCK_PORT) "clk"
 set ::env(CLOCK_NET) "clk"
-set ::env(CLOCK_PERIOD) "10.0"
-set ::env(MAX_FANOUT_CONSTRAINT) 10
+set ::env(CLOCK_PERIOD) "8"
+#set ::env(MAX_FANOUT_CONSTRAINT) 5
+set ::env(SYNTH_BUFFERING) 1
 set ::env(FP_SIZING) relative
-#set ::env(DIE_AREA) "0 0 300 300"
-set ::env(FP_CORE_UTIL) 30
+set ::env(FP_CORE_UTIL) 50
 set ::env(PL_TARGET_DENSITY) 0.55
 set ::env(GLB_RT_MAXLAYER) 5
 set ::env(MAGIC_DRC_USE_GDS) 1
+set ::env(DIODE_INSERTION_STRATEGY) 6
+set ::env(IO_PCT) 0
